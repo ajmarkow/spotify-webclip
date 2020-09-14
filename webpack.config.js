8039449d306c7b840b694ddb2631e1a2df59a9d4
Alexander Markow <alexanderjmarkow@gmail.com>
2020-09-14 13:57:51 -0700

    Update webpack config.

---
+++ webpack.config.js
@@ -0,0 +1,37 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+
+module.exports = {
+  entry: "./index.js",
+  output: {
+    filename: "bundle.js",
+    path: path.resolve(__dirname, "dist"),
+  },
+  devtool: "eval-source-map", // new line
+  devServer: {
+    // new line
+    contentBase: "./dist", // new line
+  },
+  plugins: [
+    new CleanWebpackPlugin(),
+    new HtmlWebpackPlugin({
+      title: "Spotify Webclip",
+      template: "./src/index.html",
+      inject: "body",
+    }),
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: ["style-loader", "css-loader"],
+      },
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        loader: "eslint-loader",
+      },
+    ],
+  },
+};
