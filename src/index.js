d09a419b172dea1ea8b41e5fe9ba6362c9a3a1b8
Joseph Karnafel <josephkarnafel@gmail.com>
2020-09-16 11:14:59 -0700

    begin adding output for successful api call

--- src/index.js
+++ src/index.js
@@ -3,69 +3,19 @@ import "bootstrap";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./css/styles.css";
 import Convert from "./Recognize.js";
-import base64Encoder from "./toBase64.js";
+
+function showResults(obj){
+  $("#song-title").text(obj.title);
+  $("#artist").text(`By: ${obj.spotify.artists[0].name}`)
+}
 
 $(document).ready(function () {
-  let client = filestack.init(process.env.FILESTACKAPIKEY);
-  const options = {
-    displayMode: "inline",
-
-    container: "#inline",
-  };
-
-  client.picker(options).open();
   $("#musicsubmit").click(function (event) {
     event.preventDefault();
-    // const client = filestack.init(process.env.FILESTACKAPIKEY);
-    base64Encoder(event);
-    let url = '$("input:file").val()';
+    let url = "https://audd.tech/example1.mp3"//$("input:file").val();
     Convert.getSong(url).then(function (response) {
-      console.log(response);
+      console.log(response.result)
+      showResults(response.result)
     });
   });
 });
-
-// function getElements(response) {
-//   if (response.main) {
-//     $('.showHumidity').text(`The humidity in ${response.name} is ${response.main.humidity}%`);
-//     $('.showTemp').text(`The temperature in Kelvins is ${response.main.temp} degrees.`);
-//   } else {
-//     $('.showErrors').text(`There was an error: ${response.message}`);
-//   }
-// }
-
-// $(document).ready(function() {
-//   $('#weatherLocation').click(function() {
-//     let city = $('#location').val();
-//     clearFields();
-//     WeatherService.getWeather(city)
-//       .then(function(response) {
-//         getElements(response);
-//       });
-//   });
-// });c
-
-// Example async function
-
-// function currencyEX(response, input) {
-//   if (response.rates) {
-//
-//     $('#currencyShow').text(`The currency of  the ${country} is ${conversionResult}`);
-
-//   } else {
-//     console.log(`There was an error: ${response}`)
-//     $('#showErrors').text(`There was an error: ${response}`);
-//   }
-// }
-
-// async function spotifyCall(input) {
-//   const response = await RecognizeSpotify.getSong(input);
-//   currencyEX(response, input);
-// }
-
-// $(document).ready(function() {
-//   $('#musicsubmit').click(function() {
-//     let input = $("input:file").val();
-//     spotifyCall(input);
-//   });
-// });
