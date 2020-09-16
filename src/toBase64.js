2b947fbc50c2257a6e82278ad3dfc69df7649fe3
AJ Markow <alexanderjmarkow@gmail.com>
2020-09-16 08:50:11 -0700

    Update inputs on base64 function.

--- src/toBase64.js
+++ src/toBase64.js
@@ -1,17 +1,19 @@
-export const base64Encoder = (event) => {
-  let reader = new FileReader();
-  try {
-    const file = event.target.files[0];
-    reader.readAsArrayBuffer(file);
-    reader.onload = (file) => {
-      let encoded = reader.result.split(",")[1];
-      console.log(encoded);
-      return encoded;
-    };
-  } catch (error) {
-    alert("error");
+export default class base64Encoder {
+  static readFile(input) {
+    let reader = new FileReader();
+    try {
+      const file = input.files[0];
+      reader.readAsArrayBuffer(file);
+      reader.onload = (file) => {
+        let encoded = reader.result.split(",")[1];
+        console.log(encoded);
+        return encoded;
+      };
+    } catch (error) {
+      alert("error");
+    }
   }
-};
+}
 
 // var myArray = new ArrayBuffer(512);
 // var longInt8View = new Uint8Array(myArray);
