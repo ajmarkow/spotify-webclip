bf278a9b84d7e9e053f69f9d084b5991d7ce67c8
AJ Markow <alexanderjmarkow@gmail.com>
2020-09-16 14:43:14 -0700

    Remove extra code from index.js

--- src/index.js
+++ src/index.js
@@ -2,8 +2,7 @@ import $ from "jquery";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./css/styles.css";
 import Convert from "./Recognize.js";
-import base64Encoder from "./toBase64.js";
-const fileInput = $("#fileupload").val();
+import getFilestackURL from "./fileupload.js";
 
 $(document).ready(function () {
   // eslint-disable-next-line no-undef
@@ -18,22 +17,13 @@ $(document).ready(function () {
   };
 
   client.picker(options).open();
-  client.storeURL(transformedUrl).then((res) => console.log(res));
   $("#musicsubmit").click(function (event) {
     event.preventDefault();
-    console.log(client.filesUploaded[0].url);
-    // const client = filestack.init(process.env.FILESTACKAPIKEY);
-    base64Encoder(event);
-    let url = '$("input:file").val()';
+    let url = getFilestackURL();
     Convert.getSong(url).then(function (response) {
       console.log(response);
     });
   });
-  function getFilestackURL(result) {
-    const fileData = result.filesUploaded[0].url;
-    console.log(fileData);
-    return fileData;
-  }
 });
 
 // function getElements(response) {
@@ -44,39 +34,3 @@ $(document).ready(function () {
 //     $('.showErrors').text(`There was an error: ${response.message}`);
 //   }
 // }
-
-// $(document).ready(function() {
-//   $('#weatherLocation').click(function() {
-//     let city = $('#location').val();
-//     clearFields();
-//     WeatherService.getWeather(city)
-//       .then(function(response) {
-//         getElements(response);
-//       });
-//   });
-// });c
-
-// Example async function
-
-// function currencyEX(response, input) {
-//   if (response.rates) {
-//
-//     $('#currencyShow').text(`The currency of  the ${country} is ${conversionResult}`);
-
-//   } else {
-//     console.log(`There was an error: ${response}`)
-//     $('#showErrors').text(`There was an error: ${response}`);
-//   }
-// }
-
-// async function spotifyCall(input) {
-//   const response = await RecognizeSpotify.getSong(input);
-//   currencyEX(response, input);
-// }
-
-// $(document).ready(function() {
-//   $('#musicsubmit').click(function() {
-//     let input = $("input:file").val();
-//     spotifyCall(input);
-//   });
-// });
