46e042c91dc0d51d9554d82ebcfadef0735cd67b
Joseph Karnafel <josephkarnafel@gmail.com>
2020-09-16 11:59:07 -0700

    add feature that shows album cover and preview of song upon successful api call

--- src/index.js
+++ src/index.js
@@ -5,14 +5,30 @@ import "./css/styles.css";
 import Convert from "./Recognize.js";
 
 function showResults(obj){
-  $("#song-title").text(obj.title);
-  $("#artist").text(`By: ${obj.spotify.artists[0].name}`)
+  if (obj.spotify){
+    $("#song-title").text(obj.title);
+
+    if (obj.spotify.artists.length > 1) {
+      let artistsArray = [];
+      for (let i = 0; i < obj.spotify.artists.length; i++){
+        artistsArray.push(obj.spotify.artists[i].name)
+      }
+      $("#artist").text(`By: ${artistsArray.join(", ")}`)
+    } else {
+      $("#artist").text(`By: ${obj.spotify.artists[0].name}`)
+    }
+    $("#album-cover").attr("src", obj.spotify.album.images[1].url)
+    $("#preview").show();
+    $("#preview").attr("src", obj.spotify.preview_url)
+  } else {
+    $("#song-title").text("Could not find Spotify data.");
+  }
 }
 
 $(document).ready(function () {
   $("#musicsubmit").click(function (event) {
     event.preventDefault();
-    let url = "https://audd.tech/example1.mp3"//$("input:file").val();
+    let url = "https://teamweek1epicodus.s3-us-west-2.amazonaws.com/song-2.mp3"//$("input:file").val();
     Convert.getSong(url).then(function (response) {
       console.log(response.result)
       showResults(response.result)
