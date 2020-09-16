e00d8135909ababa9c46499e06709614ac9f3c6b
AJ Markow <alexanderjmarkow@gmail.com>
2020-09-16 14:39:48 -0700

    add function that returns property of file upload result.

--- src/fileupload.js
+++ src/fileupload.js
@@ -1,3 +1,5 @@
-export default class fileUploader {
-  client.onFileUploadFinished 
+export default function getFilestackURL(result) {
+  const fileData = result.filesUploaded[0].url;
+  console.log(fileData);
+  return fileData;
 }
